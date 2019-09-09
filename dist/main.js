82763ff675cb605aad898353f11f9c5c8f060431
Dmitry <doc-chuprina@yandex.ru>
2019-09-09 16:08:44 +0300

    sync

--- dist/main.js
+++ dist/main.js
@@ -18,8 +18,14 @@ function bootstrap() {
         const fs = require('fs');
         const app = yield core_1.NestFactory.create(app_module_1.ApplicationModule, {
             cors: true,
+            httpsOptions: {
+                cert: fs.readFileSync('ca.crt'),
+                key: fs.readFileSync('ca.key'),
+                requestCert: false,
+                rejectUnauthorized: false
+            }
         });
-        const port = process.env.PORT || 8888;
+        const port = process.env.PORT || 8080;
         const options = new swagger_1.DocumentBuilder()
             .setTitle('Books store')
             .setDescription('WebAPI for selling books and magazines')
@@ -29,7 +35,7 @@ function bootstrap() {
         const document = swagger_1.SwaggerModule.createDocument(app, options);
         swagger_1.SwaggerModule.setup('swagger', app, document);
         app.useGlobalFilters(new common_1.ExceptionHandlerFilter());
-        yield app.listen(port);
+        yield app.listen(port, '10.10.0.66');
     });
 }
 bootstrap();
